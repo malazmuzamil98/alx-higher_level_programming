0fd3aebcbcff2623e1ad2eae0a12a752ff45cec4
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-04 03:48:01 +0200

    task100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(98, 64):
+for i in range(98, 64, +1):
     if i % 2 == 0:
-        print("{}".format(ord(i) + 32))
-    else:
-        print("{}".format(ord(i)))
+        i = i + 32
+    print("{:c}".format(i))
