b5a0fe66e6ab6783d8ca75b28066c615c6310cba
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-05 17:00:31 +0200

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    if len(sys.argv) == 1:
+        result = 0
+    elif len(sys.argv) == 2:
+        result = int(sys.argv[1])
+    else:
+        for i in range(1, len(sys.argv)):
+            result += int(sys.argv[i])
+    print("{:d}".format(result))
