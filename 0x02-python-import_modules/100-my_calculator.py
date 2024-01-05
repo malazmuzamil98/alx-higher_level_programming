d269427fdfae65b0a6b5ea4771422c59862bba3b
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-05 20:20:31 +0200

    task9

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,45 +3,22 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
     if len(sys.argv) < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif sys.argv[2] == "+":
-        print(
-            "{:d} {} {:d} = {:d}".format(
-                int(sys.argv[1]),
-                sys.argv[2],
-                int(sys.argv[3]),
-                add(int(sys.argv[1]), int(sys.argv[3])),
-            )
-        )
-    elif sys.argv[2] == "-":
-        print(
-            "{:d} {} {:d} = {:d}".format(
-                int(sys.argv[1]),
-                sys.argv[2],
-                int(sys.argv[3]),
-                sub(int(sys.argv[1]), int(sys.argv[3])),
-            )
-        )
-    elif sys.argv[2] == '*':
-        print(
-            "{:d} {} {:d} = {:d}".format(
-                int(sys.argv[1]),
-                sys.argv[2],
-                int(sys.argv[3]),
-                mul(int(sys.argv[1]), int(sys.argv[3])),
-            )
-        )
-    elif sys.argv[2] == "/":
-        print(
-            "{:d} {} {:d} = {:d}".format(
-                int(sys.argv[1]),
-                sys.argv[2],
-                int(sys.argv[3]),
-                div(int(sys.argv[1]), int(sys.argv[3])),
-            )
-        )
+    elif op == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == "/":
+        if b == 0:
+            sys.exit(1)
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    elif op == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
