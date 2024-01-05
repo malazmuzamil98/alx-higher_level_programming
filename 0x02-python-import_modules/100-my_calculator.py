12fd07be298ef4b8f70211284b7657b3265ba881
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-05 18:49:16 +0200

    task7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif sys.argv[2] == '+':
+        print("{:d} {:c} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), add(int(sys.argv[1], int(sys.argv[3])))))
+    elif sys.argv[2] == '-':
+        print("{:d} {:c} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), sub(int(sys.argv[1], int(sys.argv[3])))))
+    elif sys.argv[2] == '*':
+        print("{:d} {:c} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), mul(int(sys.argv[1], int(sys.argv[3])))))
+    elif sys.argv[2] == '/':
+        print("{:d} {:c} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), div(int(sys.argv[1], int(sys.argv[3])))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
