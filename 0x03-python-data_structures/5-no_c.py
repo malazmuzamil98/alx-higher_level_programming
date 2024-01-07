1bfbeefa75b6df526cb1a0b90f5432efda3c16e4
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-07 12:30:52 +0200

    task 0 to 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    un_char = ['c', 'C']
+    removed_char_str = ""
+    for char in my_string:
+        if char not in un_char:
+           removed_char_str += char
+    return (removed_char_str)
