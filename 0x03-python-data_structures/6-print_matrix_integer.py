8337d3d7590f152246d8a48c9867d544a9558f3f
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-07 13:15:44 +0200

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
+        print()
         pass
     else:
         for row in matrix:
@@ -11,3 +12,13 @@ def print_matrix_integer(matrix=[[]]):
                     print("{:d}".format(element), end=' ')
                 else:
                     print("{:d}".format(element))
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
