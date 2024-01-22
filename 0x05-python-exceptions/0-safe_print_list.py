6ec67ec1ae8cc35564d5ec0e470acf6a601a249b
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-22 19:59:04 +0200

    t0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,3 +9,12 @@ def safe_print_list(my_list=[], x=0):
         return count
     except IndexError:
         return count
+
+my_list = [1, 2, 3, 4, 5, l, k]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
