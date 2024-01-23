208bb4511beaac77a08dd83e6ed3e7dcdffa6c1c
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-23 03:58:46 +0200

    t7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        sys.stderr.write("Exception: {}\n".format(i))
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
     else:
         return True
