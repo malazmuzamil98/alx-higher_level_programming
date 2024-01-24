e8143bc9db7c5585976f6c877b66aa462cc1f311
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-24 04:02:48 +0200

    t5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,66 @@
 #!/usr/bin/python3
+"""A module for a square class"""
+
+
+class Square:
+    """A class that defines a square"""
+
+    def __init__(self, size=0):
+        """initialization point of instunces
+
+        Args:
+            size : size of the square
+        Raises:
+            TypeError : if size is not an integer
+            ValueError : if size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns:
+            integer: returns the area of the square which is hight * width
+            in the square case they are equal and equals the size
+        """
+
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the square size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square
+
+        Args:
+            value (int): square size
+
+        Raises:
+            TypeError: if the value is integer
+            ValueError: if the value is less than 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """_summary_
+        """
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
