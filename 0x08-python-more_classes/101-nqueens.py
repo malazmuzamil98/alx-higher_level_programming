722ba4f02c27e6e773a2cb5ea354b27b664ab884
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-30 01:04:47 +0200

    t10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -39,3 +39,72 @@ def print_result(m_queen, nqueen):
         res.append([i, m_queen[i]])
 
     print(res)
+
+
+"""
+This module contains an algorithm that resolves the N-Queen puzzle
+using backtracking
+"""
+
+
+def Queen(m_queen, nqueen):
+    """_summary_
+
+    Args:
+        m_queen (_type_): _description_
+        nqueen (_type_): _description_
+    """
+
+    if nqueen is len(m_queen):
+        print_result(m_queen, nqueen)
+        return
+
+    m_queen[nqueen] = -1
+
+    while((m_queen[nqueen] < len(m_queen) - 1)):
+
+        m_queen[nqueen] += 1
+
+        if isSafe(m_queen, nqueen) is True:
+
+            if nqueen is not len(m_queen):
+                Queen(m_queen, nqueen + 1)
+
+
+"""
+This module contains an algorithm that resolves the N-Queen puzzle
+using backtracking
+"""
+
+
+def solveNQueen(size):
+    """_summary_
+
+    Args:
+        size (_type_): _description_
+    """
+
+    m_queen = [-1 for i in range(size)]
+
+    Queen(m_queen, 0)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        size = int(sys.argv[1])
+    except Exception:
+        print("N must be a number")
+        sys.exit(1)
+
+    if size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solveNQueen(size)
