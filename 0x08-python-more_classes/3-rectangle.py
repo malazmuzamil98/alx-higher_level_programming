1f776f5fe22da5273a7291bfd158cd0188cb0cc6
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-29 21:36:48 +0200

    t3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -98,11 +98,11 @@ my_rectangle = Rectangle(2, 4)
 print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
 print(str(my_rectangle))
-#print(repr(my_rectangle))
+print(repr(my_rectangle))
 
 print("--")
 
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle)
-#print(repr(my_rectangle))
+print(repr(my_rectangle))
