0836195f6f2d30d8b43a0cb707aee11c4d04f6c0
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-02-20 17:52:42 +0200

    t4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+
+
+def inherits_from(obj, a_class):
+    """_summary_"""
+    return isinstance(obj, a_class)
+
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
