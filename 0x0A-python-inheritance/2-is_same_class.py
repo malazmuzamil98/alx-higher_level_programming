7e640457e4005290478a7654233972de497143a2
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-02-20 17:42:26 +0200

    t2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Summary"""
+
+
+def is_same_class(obj, a_class):
+    """_summary_"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
