bbeb30408c57f950fe866789bf8769254f6812f7
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-02-20 16:06:51 +0200

    t1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""A class that inherits from the list class"""
+
+
+class MyList(list):
+    """A class that inherits from the list class"""
+
+    def print_sorted(self):
+        """Prints the list in sorted order"""
+        print(sorted(self))
