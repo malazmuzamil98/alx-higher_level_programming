11daa68b44a7e43837257388bf4642f14816772d
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-02-20 23:08:45 +0200

    t101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""_summary_"""
+
+
+def add_attribute(obj, name, value):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        name (_type_): _description_
+        value (_type_): _description_
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
