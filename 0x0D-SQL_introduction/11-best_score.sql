6c4778a3dfbfac3ea24d9abf8415321f42ebb5ef
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-03-14 21:01:32 +0200

    t 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,6 @@
 -- lists all databases of MySQL server
 -- using commnad
-SELECT score, name FROM second_table WHERE score >= 10 OREDER BY DECS score;
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
