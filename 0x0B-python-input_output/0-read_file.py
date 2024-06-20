98be91f98038d68eb15d57285c6070da561e2b13
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-06-21 02:00:48 +0300

    all

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    with open("filename", "r"):
+        file_content = filename.read()
+    print(file_content)
+
+
+read_file("my_file_0.txt")
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """_summary_"""
 
 
+import json
 class Base:
     """_summary_"""
 
@@ -19,3 +20,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """_summary_
+
+        Args:
+            list_dictionaries (_type_): _description_
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """_summary_
+
+        Args:
+            list_objs (_type_): _description_
+        """
+
+        if list_objs is None:
+            list_objs = []
+        class_name = cls.__name__
+        file_name = class_name + ".json"
+        dict_list = [obj.to_dictionary() for obj in list_objs]
+        with open(file_name, "w") as file:
+            file.write(cls.to_json_string(dict_list))
