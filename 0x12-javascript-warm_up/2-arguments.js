952856b6777524af702e82a895084e729b25f078
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-06-20 16:21:42 +0300

    t2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// args count
+const args = process.argv.length - 2;
+
+// print a message
+if (args === 0) {
+    console.log('No argument');
+}
+else if (args === 1) {
+    console.log('Argument found');
+}
+else {
+    console.log('Argument found');
+}
