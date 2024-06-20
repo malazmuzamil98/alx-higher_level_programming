deb4ec3c38024c614a6ce46162f22437e963d414
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-06-20 16:24:30 +0300

    t2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,11 +4,9 @@ const args = process.argv.length - 2;
 
 // print a message
 if (args === 0) {
-    console.log('No argument');
-}
-else if (args === 1) {
-    console.log('Argument found');
-}
-else {
-    console.log('Argument found');
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
